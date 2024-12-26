db9344d21b54446481b8d3fbf1a9747502f87811
cc-Mehdi <57840939+cc-Mehdi@users.noreply.github.com>
2024-12-26 14:59:10 +0330

    Init Project

---
+++ src/db_class_office_project/Database Scripts/01_Create_DatabaseAndTables.sql
@@ -0,0 +1,16 @@
+﻿Create Database FileCirculationManagementSystem_DB
+
+create table FileStatus (
+Id int primary key Identity(1,1) not null,
+Title nvarchar(256)
+);
+
+Create Table Files (
+Id int primary key Identity(1, 1) not null,
+FileStatus_Id int Unique,
+CaseId int,
+FullName nvarchar(256),
+SubscriptionCode nvarchar(256),
+
+foreign key (FileStatus_Id) References FileStatus(Id)
+);
---
+++ src/db_class_office_project/Database Scripts/02_Seed_SampleData.sql
@@ -0,0 +1,16 @@
+﻿-- Insert sample records into FileStatus
+INSERT INTO FileStatus (Title) VALUES
+('ریاست'),
+('کارشناس گشت و بازرسی'),
+('کارشناس منابع آب'),
+('بایگانی'),
+('کمیسیون');
+GO
+
+-- Insert sample records into Files
+INSERT INTO Files (FileStatus_Id, CaseId, FullName, SubscriptionCode) VALUES
+(1, 2225, 'رضا بختیار', '123456'), -- پرونده در ریاست
+(4, 2226, 'علی رضایی', '654321'), -- پرونده در بایگانی
+(2, 2227, 'مریم احمدی', '111222'), -- پرونده در گشت و بازرسی
+(5, 2228, 'حسین معینی', '333444'); -- پرونده در کمیسیون
+GO
